3a07daa5c1ae522ff31f9cd2f4e59552fbd2ee60
Reham Hamdi <rehamhamdi53@gmail.com>
2025-10-27 22:36:26 +0300

    Implement Fluent Validation with auto valadition for Employee model

--- WorkHub.DAL/Models/Employee.cs
+++ WorkHub.DAL/Models/Employee.cs
@@ -7,6 +7,9 @@
         public string FullName { get; set; }
         public DateTime DateOfBirth { get; set; }
         public string? JobTitle { get; set; }
+        //[ForeignKey("Department")] this is only needed if we use a different naming convention.
+        // EF Core automatically recognizes foreign keys when property names follow the pattern <NavigationPropertyName>Id .
+        // For example, 'DepartmentId' links to the 'Department' navigation property.
         public int DepartmentId { get; set; }
 
         // Navigation Property
